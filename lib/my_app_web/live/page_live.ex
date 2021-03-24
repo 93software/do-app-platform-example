23268a7cff2308cec7060bdf0f889b46f00ecb49
Justin Tormey <jrtormey@gmail.com>
2021-03-23 21:49:01 -0500

    Add splash page

--- lib/my_app_web/live/page_live.ex
+++ lib/my_app_web/live/page_live.ex
@@ -1,8 +1,10 @@
 defmodule MyAppWeb.PageLive do
   use MyAppWeb, :live_view
 
+  @github_url "https://github.com/93software/do-app-platform-example"
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :github_url, @github_url)}
   end
 end
